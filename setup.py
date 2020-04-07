302f1809da668f84c5ca882da40aeb43b0abdf6b
Nick Hess <hessproject@gmail.com>
2020-04-06 22:40:32 -0700

    Update setup.py url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description='Client for SURE Acme API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/hessproject/sure_interview',
+    url='https://github.com/hessproject/acme-volcano-insurance',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
